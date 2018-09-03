462d5550945febc5b16922da210c8e2c80f6156b
ahribori <ahribori@gmail.com>
2018-09-04 00:11:21 +0900

    Dispatch type 해결

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import SampleComponent from '../components/SampleComponent';
 import { RootState } from '../store';
-import { fetchSample } from '../store/Sample';
-import { Dispatch } from 'redux';
+import { fetchSample, SampleState } from '../store/Sample';
+import { AnyFunction } from '../models/common';
 
 interface HomeProps {
-    dispatch: Dispatch<any>
+    sample: SampleState;
+    fetchSample: () => void;
 }
 
 class Home extends React.Component<HomeProps> {
@@ -19,10 +20,16 @@ class Home extends React.Component<HomeProps> {
     }
 
     public async componentDidMount() {
-        await this.props.dispatch(fetchSample());
+        await this.props.fetchSample();
     }
 }
 
 const mapStateToProps = (state: RootState) => state;
 
-export default connect(mapStateToProps)(Home);
+const mapDispatchToProps = (dispatch: AnyFunction) => {
+    return {
+        fetchSample: () => dispatch(fetchSample()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
